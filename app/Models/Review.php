cbc234a8a2d4813f01daf4dba7fc863dbba2c054
Mert Bağ <bagmert1996@gmail.com>
2024-10-08 02:01:05 +0300

    Scoped Review Resource

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -29,6 +29,8 @@ class Review extends Model
 
     protected static function booted()
     {
+        static::created(fn(Review $review) => cache()->forget('book:' . $review->book_id));
+
         static::updated(fn(Review $review) => cache()->forget('book:' . $review->book_id));
 
         static::deleted(fn(Review $review) => cache()->forget('book:' . $review->book_id));
